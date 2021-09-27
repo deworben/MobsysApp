a0fcbe5df49c6d533b2f5746150ca60b0d861145
Jingyuan Tu <jingyuantu@outlook.com>
2021-09-27 11:04:18 +1000

    Add data loaders.

---
+++ experiments/main.py
@@ -0,0 +1,46 @@
+import json
+
+
+# reading label descriptions
+ontology = {}
+ontology_ids = {}
+with open("./audioset/ontology.json", "r") as f:
+    ontology = json.load(f)
+    ontology = { o["name"]: o for o in ontology }
+    ontology_ids = { o["id"]: o["name"] for o in ontology.values() }
+
+
+# reading train video metadata
+train_metadata = {}
+with open("./audioset/train_metadata.csv", "r") as f:
+    line = f.readline()
+    while line:
+        items = line.strip().split(", ")
+        labels = items[3].strip("\"").split(",")
+        for l in labels:
+            key = ontology_ids[l]
+            if key in train_metadata:
+                train_metadata[key].append((items[0], float(items[1]), float(items[2])))
+            else:
+                train_metadata[key] = [(items[0], float(items[1]), float(items[2]))]
+        line = f.readline()
+
+
+# reading test video metadata
+test_metadata = {}
+with open("./audioset/test_metadata.csv", "r") as f:
+    line = f.readline()
+    while line:
+        items = line.strip().split(", ")
+        labels = items[3].strip("\"").split(",")
+        for l in labels:
+            key = ontology_ids[l]
+            if key in test_metadata:
+                test_metadata[key].append((items[0], float(items[1]), float(items[2])))
+            else:
+                test_metadata[key] = [(items[0], float(items[1]), float(items[2]))]
+        line = f.readline()
+
+
+
+print(test_metadata["Laughter"])
