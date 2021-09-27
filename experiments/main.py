36dac9ba60268c0a7a251d284a7c5cd6e9023c74
Jingyuan Tu <jingyuantu@outlook.com>
2021-09-27 15:15:43 +1000

    MFCC and Spectrogram Features Cache

--- experiments/main.py
+++ experiments/main.py
@@ -1,11 +1,22 @@
 import os
 from pathlib import Path
 import json
-from youtube_dl import YoutubeDL, downloader
+from youtube_dl import YoutubeDL
 from pydub import AudioSegment
+import librosa
+import librosa.display
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-# program config
+# BUG:
+# 1. Currently only train set is being used.
+
+# NOTE:
+# 1. We may train on other categories of sound in the future.
+
+
+# program configurations
 categories = ["Laughter", "Chatter"]
 
 
@@ -65,12 +76,12 @@ def download():
                     fails += 1
                     print(f"Download failed, total failures in category {c} is {fails}.")
 
+
 # crop the audio to desired length
 def crop():
     for c in categories:
         Path("./processed/" + c).mkdir(parents=True, exist_ok=True)
-        fnames = os.listdir(f"./audio/{c}")
-        for n in fnames:
+        for n in os.listdir(f"./audio/{c}"):
             print(f"Cropping {n} in {c}.")
             audio = AudioSegment.from_file(f"./audio/{c}/{n}")
             yid = n.rstrip(".m4a")
@@ -81,8 +92,40 @@ def crop():
                     start = m[1] * 1000
                     end = m[2] * 1000
             subsequence = audio[start:end]
-            subsequence.export(f"./processed/{c}/{n}.mp3", format="mp3")
-    return
+            subsequence.export(f"./processed/{c}/{yid}.mp3", format="mp3")
+
+
+# audio to mfcc features 
+def mfcc():
+    for c in categories:
+        Path("./mfcc/" + c).mkdir(parents=True, exist_ok=True)
+        for n in os.listdir(f"./processed/{c}"):
+            print(f"Extracting MFCC for {n}")
+            try:
+                x, sr = librosa.load(f"./processed/{c}/{n}")
+            except:
+                print("MFCC cannot load.")
+                continue
+            n = n.rstrip(".mp3")
+            with open(f"./mfcc/{c}/{n}.mfcc", "w+") as f:
+                json.dump(librosa.feature.mfcc(x, sr=sr).tolist(), f)
+
+
+# audio to spectrogram
+def spectrogram():
+    for c in categories:
+        Path("./spectrogram/" + c).mkdir(parents=True, exist_ok=True)
+        for n in os.listdir(f"./processed/{c}"):
+            print(f"Extracting spectrogram for {n}")
+            try:
+                x, sr = librosa.load(f"./processed/{c}/{n}")
+            except:
+                print("Spectrogram cannot load.")
+                continue
+            n = n.rstrip(".mp3")
+            with open(f"./spectrogram/{c}/{n}.spectro", "w+") as f:
+                X = librosa.stft(x)
+                json.dump(librosa.amplitude_to_db(abs(X)).tolist(), f)
 
 
 
@@ -92,5 +135,8 @@ def crop():
 # uncomment the following line to crop audio files
 # crop()
 
+# uncomment the following line to cache mfcc features
+# mfcc()
 
-
+# uncomment the following line to cache spectrogram features
+# spectrogram()
