79be5c1bc09d288a37b968324b7b9296778d82d9
Tim <tim10@bunnage.com.au>
2021-10-03 15:19:12 +1100

    Recording button saves to temp file

--- laugh_diary/lib/main.dart
+++ laugh_diary/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:flutter_sound/flutter_sound.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,7 +16,7 @@ class MyApp extends StatelessWidget {
           title: const Text("press the button"),
         ),
         body: Center(
-          child: RecordButton(),      // use the RecordButton widget
+          child: AudioRecorder(),      // use the RecordButton widget
         ),
         backgroundColor: Colors.black,
     ),
@@ -22,75 +25,184 @@ class MyApp extends StatelessWidget {
   }
 }
 
-//
-class RecordButton extends StatefulWidget {
-  const RecordButton({Key? key}) : super(key: key);
-
-  @override
-  _RecordButtonState createState() => _RecordButtonState();
-}
-
-// The button will change state depending on _recording
-class _RecordButtonState extends State<RecordButton> {
-  bool _recording = false;
-  @override
-  Widget build(BuildContext context) {
-    return TextButton(
-        onPressed: () {
-          setState(() {
-            _recording = _recording ? false : true;
-          });
-        },
-        child: _recording ? const Text("Stop Recording") : const Text("Press to Record")
-    );
-  }
-}
-
-
-
-// CODE FROM THE TUTORIAL
-//
-// class RandomWords extends StatefulWidget {
-//   const RandomWords({Key? key}) : super(key: key);
+// //
+// class RecordButton extends StatefulWidget {
+//   const RecordButton({Key? key}) : super(key: key);
 //
 //   @override
-//   _RandomWordsState createState() => _RandomWordsState();
+//   _RecordButtonState createState() => _RecordButtonState();
 // }
 //
-// class _RandomWordsState extends State<RandomWords> {
-//   final _suggestions = <WordPair>[];
-//   final _biggerFont = const TextStyle(fontSize: 18.0);
+// // The button will change state depending on _recording
+// class _RecordButtonState extends State<RecordButton> {
+//   bool _recording = false;
+//   @override
+//   Widget build(BuildContext context) {
+//     return TextButton(
+//         onPressed: () {
+//           setState(() {
+//             _recording = _recording ? false : true;
+//           });
+//         },
+//         child: _recording ? const Text("Stop Recording") : const Text("Press to Record")
+//     );
+//   }
+// }
+
+
+class AudioRecorder extends StatefulWidget {
+  const AudioRecorder({Key? key}) : super(key: key);
+
+  @override
+  _AudioRecorderState createState() => _AudioRecorderState();
+}
+
+class _AudioRecorderState extends State<AudioRecorder> {
+  FlutterSoundRecorder? _myRecorder = FlutterSoundRecorder();
+  bool _myRecorderIsInited = false;
+  bool _isRecording = false;
+  String _mPath = "";
+  final _mCodec = Codec.aacADTS;
+
+
+  @override
+  Widget build(BuildContext context) {
+    print("build button!");
+    print(_myRecorderIsInited);
+    return TextButton(
+        onPressed: () {
+          if (_myRecorderIsInited) {
+            !_isRecording ? record().then((value) {
+              setState(() {
+                _isRecording = true;
+              });
+            })
+                : stopRecorder().then((value) {
+              setState(() {
+                _isRecording = false;
+              });
+            });
+          }
+          // setState(() {
+          //   _isRecording == _isRecording ? false : true;
+          // });
+        },
+        child: _isRecording ? const Text("Stop Recording") : const Text("Press to Record")
+    );  }
+
+
+  @override
+  void initState() {
+    super.initState();
+
+
+    // CARE: openAudioSession returns a Future
+    // Do not access your FlutterSoundPlayer or FlutterSoundRecorder before the completion of the Future
+    openRecorder().then((value) {
+      setState(() {
+        _myRecorderIsInited = true;
+      });
+    });
+
+    print("recorder init");
+  }
+
+  Future<void> openRecorder() async {
+    final status = await Permission.microphone.request();
+    if (status != PermissionStatus.granted) {
+      throw RecordingPermissionException(
+          'Microphone permission is not granted');
+    }
+
+    // Get temp file
+    var tempDir = await getTemporaryDirectory();
+    _mPath = '${tempDir.path}/TEST_FILE.aac';
+
+    _myRecorder!.openAudioSession().then((value) {
+      setState(() {
+        _myRecorderIsInited = true;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _myRecorder!.closeAudioSession();
+    _myRecorder = null;
+    super.dispose();
+    print("recorder dispose");
+
+  }
+
+  Future<void> record() async {
+    await _myRecorder!.startRecorder(
+      toFile: _mPath,
+      codec: _mCodec,
+    ).then((value) {
+      print("currently recording!!");
+    });
+  }
+
+  Future<void> stopRecorder() async {
+    await _myRecorder!.stopRecorder().then((value) {
+      print("stopped recording!!");
+    });
+  }
+
+}
+
+
+//
+//
+//
+// class AudioPlayer extends StatefulWidget {
+//   const AudioPlayer({Key? key}) : super(key: key);
+//
+//   @override
+//   _AudioPlayerState createState() => _AudioPlayerState();
+// }
+//
+// class _AudioPlayerState extends State<AudioPlayer> {
+//   FlutterSoundPlayer? _myPlayer = FlutterSoundPlayer();
+//   bool _mPlayerIsInited = false;
+//   final _exampleAudioFilePathMP3 = "/";
 //
 //   @override
 //   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text("startup name generator"),
-//       ),
-//       body: _buildSuggestions(),
-//     );
+//     return Container();
 //   }
 //
-//   Widget _buildSuggestions() {
-//     return ListView.builder(
-//       padding: const EdgeInsets.all(16.0),
-//       itemBuilder: (context, i) {
-//         if (i.isOdd) return const Divider();
-//         final index = i ~/ 2;
-//         if (index >= _suggestions.length) {
-//           _suggestions.addAll(generateWordPairs().take(10));
-//         }
-//         return _buildRow(_suggestions[index]);
+//   @override
+//   void initState() {
+//     super.initState();
+//     _myPlayer!.openAudioSession().then((value) {
+//       setState(() {
+//         _mPlayerIsInited = true;
+//       });
+//     });
+//   }
+//
+//   @override
+//   void dispose() {
+//     _myPlayer!.closeAudioSession();
+//     _myPlayer = null;
+//     super.dispose();
+//   }
+//
+//   void play() async {
+//     await _myPlayer!.startPlayer(
+//         fromURI: _exampleAudioFilePathMP3,
+//         codec: Codec.mp3,
+//         whenFinished: (){ setState((){}); }
+//     );
+//     setState((){});
+//   }
+//
+//   Future<void> stopPlayer() async {
+//     if (_myPlayer != null) {
+//       await _myPlayer!.stopPlayer();
 //     }
-//     );
-//   }
-//
-//   Widget _buildRow(WordPair pair) {
-//     return ListTile(
-//       title: Text(
-//         pair.asPascalCase,
-//         style: _biggerFont,
-//       ),
-//     );
 //   }
 // }
+//
+//
